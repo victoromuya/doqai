eb521a6151490d5069d09361f73079a146aca52d
unknown <viczik16@gmail.com>
2026-04-22 10:48:35 +0100

    cor head

--- Docai_api/settings.py
+++ Docai_api/settings.py
@@ -51,6 +51,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://10.242.146.115:8080",
     "http://localhost:5173"
+    "https://doqai-frontend.vercel.app"
 ]
 
 # CORS_ALLOWED_ORIGIN_REGEXES = [
